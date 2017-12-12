221bd19c8ca7f9cfbb8738c70f299114796b1550
Jason541029 <jtr5494@gmail.com>
2017-12-12 11:53:03 +0800

    2017/12/12 浮動按鈕&存取資料&記住上次資料

--- app/src/main/java/com/boris/atm/LoginActivity.java
+++ app/src/main/java/com/boris/atm/LoginActivity.java
@@ -10,24 +10,31 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText edUserid;//ctrl alt f 抽出
+    private EditText edPasswd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        String userid = getSharedPreferences("atm", MODE_PRIVATE)
+                .getString("USERID", "");
+        edUserid = findViewById(R.id.userid);
+        edPasswd = findViewById(R.id.passwd);
+        edUserid.setText(userid);//記住上次打的
     }
-    public  void login(View view){
-        EditText edUserid = findViewById(R.id.userid);
-        EditText edPasswd =findViewById(R.id.passwd);
+
+    public void login(View view) {
         String userid = edUserid.getText().toString();
-        String password= edPasswd.getText().toString();
-        if("jack".equals(userid)&&"1234".equals(password)){
-            Toast.makeText(this,"登入成功",Toast.LENGTH_SHORT).show();
-            getIntent().putExtra("LOGIN_USERID",userid);
-            getIntent().putExtra("LOGIN_PASSWD",password);
-            setResult(RESULT_OK,getIntent());
+        String password = edPasswd.getText().toString();
+        if ("jack".equals(userid) && "1234".equals(password)) {
+            Toast.makeText(this, "登入成功", Toast.LENGTH_SHORT).show();
+            getIntent().putExtra("LOGIN_USERID", userid);
+            getIntent().putExtra("LOGIN_PASSWD", password);
+            setResult(RESULT_OK, getIntent());
             finish();
 
-        }else {
+        } else {
             new AlertDialog.Builder(this)
                     .setTitle("登入")
                     .setMessage("登入失敗")
@@ -36,7 +43,8 @@ public class LoginActivity extends AppCompatActivity {
         }
 
     }
-    public  void cancel(View view){
+
+    public void cancel(View view) {
 
     }
 }
